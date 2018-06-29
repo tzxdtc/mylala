8860df7af7c2bbb38ea3a14ed4422cae4b6852f0
tan <z_tan@acceluniverse.com>
2018-06-29 15:36:09 +0900

    複数のアクション利用

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{msg}', function ($msg) {
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {
-  font-size:16pt;
-  color:#999;
-}
-h1{
-  font-size:100pt;
-  text-align:right;
-  color:#222;
-  margin:-40px 0px -50px 0px;
-}
-</style>
-</head>
-<body>
-  <h1>Hello</h1>
-  <p>{$msg}</p>
-</body>
-</html>
-EOF;
+// Route::get('hello/{msg}', function ($msg) {
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {
+//   font-size:16pt;
+//   color:#999;
+// }
+// h1{
+//   font-size:100pt;
+//   text-align:right;
+//   color:#222;
+//   margin:-40px 0px -50px 0px;
+// }
+// </style>
+// </head>
+// <body>
+//   <h1>Hello</h1>
+//   <p>{$msg}</p>
+// </body>
+// </html>
+// EOF;
+//
+//   return $html;
+// });
 
-  return $html;
-});
+// Route::get('helloo/{id?}/{pass?}', 'helloController@index');
+Route::get('hello','helloController@index');
+Route::get('hello/other','helloController@other');
