7638a16aabbc2821e8331a80fbd4b281c43918b3
tan <z_tan@acceluniverse.com>
2018-07-09 21:20:13 +0900

    use validation

--- app/Http/Controllers/helloController.php
+++ app/Http/Controllers/helloController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Response;
 
 class helloController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-      return view('hello.index',['message'=>'Hello!']);
+      return view('hello.index',['msg'=>'type form']);
 }
+
+    public function post(Request $request){
+      $validate_rule = [
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'numeric|between:0,150',
+      ];
+      $this->validate($request, $validate_rule);
+      return view('hello.index',['msg'=>'this is right type']);
+    }
     // public function post(Request $request){
     //   return view('hello.index', ['msg'=>$request->msg]);
     // }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,44 @@
 @endsection
 
 @section('content')
-  <p>this is the article contents</p>
-  <p>Controller value<br>'message' = {{$message}}</p>
-  <p>Controller value<br>'view_message' = {{$view_message}}</p> 
+  <p>{{$msg}}</p>
+@if (count($errors) > 0)
+<p>there is a problem with your type</p>
+@endif
+<table>
+<form action="hello" method="post">
+  {{ csrf_field() }}
+
+@if ($errors->has('name'))
+
+<tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+
+@endif
+
+<tr><th>name:</th><td><input type="text"
+  name = "name" value="{{old('name')}}"></td></tr>
+
+  @if ($errors->has('mail'))
+
+  <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+
+  @endif
+
+<tr><th>mail:</th><td><input type="text"
+  name = "mail" value="{{old('mail')}}"></td></tr>
+
+  @if ($errors->has('age'))
+
+  <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
+
+  @endif
+
+<tr><th>age:</th><td><input type="text"
+  name = "age" value="{{old('age')}}"></td></tr>
+<tr><th></th><td><input type="submit"
+  name = "send"></td></tr>
+</form>
+</table>
 
 @endsection
 
