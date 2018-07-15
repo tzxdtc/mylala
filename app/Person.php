5a1a953c18a303a59a45c3d61cb52ac0d11636f6
tan <z_tan@acceluniverse.com>
2018-07-15 22:37:51 +0900

    make global scope

--- app/Person.php
+++ app/Person.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -25,9 +26,6 @@ class Person extends Model
 
     protected static function boot(){
       parent::boot();
-
-      static::addGlobalScope('age',function (Builder $builder){
-        $builder->where('age','>',30);
-      });
+      static::addGlobalScope(new ScopePerson);
     }
 }
